55349d859118db98a22ac7cc0ff700479f19fb22
Helena Zimmermann <hteodora52@gmail.com>
2025-05-20 19:29:15 -0300

    Ajuste para importar no intellij

--- DesafioSerasa/DesafioSerasa/desafio/baseB/src/main/java/com/serasa/baseb/repository/UsuarioBaseBRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.serasa.baseb.repository;
-
-import com.serasa.baseb.usuario.UsuarioBaseB;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface UsuarioBaseBRepositorio extends MongoRepository<UsuarioBaseB, String> {
-    Optional<UsuarioBaseB> findByCpf(String cpf);
-}
